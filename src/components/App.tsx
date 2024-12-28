923751012d2921d3e7ea1fe36efb50d925ee68b2
Sriveer Neerukonda <Sriveer.NEERUKONDA@outlook.com>
2024-12-28 16:13:29 +0530

    feat: add text styling for project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function App() {
     return(
-        <h1>Hello world from react app!!</h1>
+        <>
+            <div className='scale'></div>
+            <h1 className='heading'>Sriveer Neerukonda</h1>
+            <h2 className='subtitle red'>Video Game Programmer & Full Stack Web Developer</h2>
+            <p className='body-copy'>I am a young adult.</p>
+        </>
+        
     );
 }
